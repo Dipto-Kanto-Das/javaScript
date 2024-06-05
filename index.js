33a30c9e2761c4ce0085fb9ce6bcdd315205c3a6
Dipto2512 <dipto2512@gmail.com>
2024-06-05 18:48:41 +0600

    index java

--- index.js
+++ index.js
@@ -266,18 +266,25 @@
 // }
 // //10-calculate electricity bill from given condition
 
-let unit = 252
-let amount=0;
-if (unit <= 50) {
-    amount = unit * .50;
+// let unit = 252
+// let amount=0;
+// if (unit <= 50) {
+//     amount = unit * .50;
+// }
+// else if (unit <= 150) {
+//     amount = ((50 * .50) + (unit - 50) * .75);
+// }
+// else if ((unit <= 250)) {
+//     amount =100 +(unit-150) *1.20;
+// }
+// else if(unit>250) {
+//     amount = 220 + (unit - 250) * 1.50;
+// }
+// console.log(amount+55);
+
+//LOOP
+
+
+for (let j=1;j<=21;j++){
+    console.log(j,".Dipto");
 }
-else if (unit <= 150) {
-    amount = ((50 * .50) + (unit - 50) * .75);
-}
-else if ((unit <= 250)) {
-    amount =100 +(unit-150) *1.20;
-}
-else if(unit>250) {
-    amount = 220 + (unit - 250) * 1.50;
-}
-console.log(amount+55);
